fc856a089ccfa9954ce4a105e32c0017fe2de3b7
brant-matheus <brantccm@hotmail.com>
2024-03-15 09:42:47 -0300

    revised binary search code done

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,6 @@
+# MUST BE a sorted list.
+# 0(log n) worst case. log base 2 of n.
+
 size_list = (1*(10**2))+1
 sorted_list = [i for i in range(1, size_list)] #generating a list from 1 up to size_list.
 
@@ -21,5 +24,5 @@ def binary_search(sorted_list, item):
             high = mid - 1
     
     return None #item not in list.
-
-print(binary_search(sorted_list, 0))
+item = 12
+print(binary_search(sorted_list, item))
